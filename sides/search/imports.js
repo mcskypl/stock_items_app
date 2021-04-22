76b6e72684cf9aa3c2605d29794fcf6fc652068a
Sebastian Westfal <mcskypl@gmail.com>
2021-04-22 19:05:50 +0200

    Add and list fn in one

---
+++ sides/search/imports.js
@@ -0,0 +1,16 @@
+const list = document.querySelector('.list');
+const szukaj = document.querySelector('.szukaj');
+const alertSuccess = document.querySelector('.alert-success');
+const btnCloseSuccess = document.querySelector('.btn-close-alert-success');
+const bgModal = document.querySelector('.bgModal');
+const deleteButton = document.querySelector('.deleteButton');
+const stayButton = document.querySelector('.stayButton');
+const brak = document.querySelector('.brak');
+const addIndex = document.querySelector('.addIndex');
+
+const form = document.querySelector('.form');
+const badgeIndex = document.querySelector('.badgeIndex');
+const alertWarningAdd = document.querySelector('.alert-warning-add');
+const alertSuccessAdd = document.querySelector('.alert-success-add');
+const btnCloseWarning = document.querySelector('.btn-close-alert-warning');
+const btnCloseSuccessAdd = document.querySelector('.btn-close-alert-success');
--- sides/search/search.js
+++ sides/search/search.js
@@ -1,97 +1,144 @@
-const list = document.querySelector('.list');
-const szukaj = document.querySelector('.szukaj');
-const alertSuccess = document.querySelector('.alert-success');
-const btnCloseSuccess = document.querySelector('.btn-close-alert-success');
-const bgModal = document.querySelector('.bgModal');
-const deleteButton = document.querySelector('.deleteButton');
-const stayButton = document.querySelector('.stayButton');
-
 btnCloseSuccess.addEventListener('click', () => {
     alertSuccess.classList.remove('show');
 })
 
+const addIndexFn = (indexValue) => {
+    addIndex.addEventListener('click', () => {
+        console.log(indexValue);
+        form.classList.remove('hiddenClass');
+        szukaj.classList.add('hiddenClass');
+        addIndex.classList.add('hiddenClass');
+
+        btnCloseSuccessAdd.addEventListener('click', () => {
+            alertSuccessAdd.classList.remove('show');
+            alertSuccessAdd.style.zIndex = '1';
+        })
+
+        btnCloseWarning.addEventListener('click', () => {
+            alertWarningAdd.classList.remove('show');
+            alertWarningAdd.style.zIndex = "1";
+        })
+
+        badgeIndex.innerHTML = `<h1>${indexValue}</h1>`;
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let i = false;
+            let index = indexValue;
+
+            db.collection('magazyn').doc('mag4').collection(index).get().then((snapshot) => {
+                snapshot.docs.forEach(doc => {
+                    if (form.miejsce.value.toUpperCase() === doc.data().miejsce && form.partia.value === doc.data().partia) i = true;
+                })
+
+                if (!i) {
+                    db.collection('magazyn').doc('mag4').collection(index).add({
+                        miejsce: form.miejsce.value.toUpperCase(),
+                        partia: form.partia.value
+                    })
+                    alertSuccess.classList.add('show');
+                    alertSuccess.style.zIndex = '10';
+                } else {
+                    alertWarning.style.zIndex = "10";
+                    alertWarning.classList.add('show');
+                }
+
+                form.partia.value = '';
+            });
+        })
+    })
+}
+
+// *-------------------------------------------------------------------------------------------- RENDER LIST
+const renderListFn = (doc) => {
+    //console.log(doc);
+    let li = document.createElement('div');
+    // let index = document.createElement('span');
+    let partia = document.createElement('span');
+    let miejsce = document.createElement('span');
+    let trash = document.createElement('i');
+
+    li.setAttribute('data-id', doc.id);
+    li.className = 'list2';
+    miejsce.className = 'miejsce';
+    trash.className = 'bi bi-trash';
+
+    // index.textContent = ai;
+    partia.textContent = doc.data().partia;
+    miejsce.textContent = doc.data().miejsce;
+
+    // li.appendChild(index);
+    li.appendChild(miejsce);
+    li.appendChild(partia);
+    li.appendChild(trash);
+
+    list.appendChild(li);
+
+    // *-------------------------------------------------------------------------------------------- DELETING DATA
+    trash.addEventListener('click', (e) => {
+        e.stopPropagation();
+
+        let id = e.target.parentElement.getAttribute('data-id');
+        bgModal.classList.remove('hiddenClass');
+
+        deleteButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log(id);
+
+            //console.log(szukaj.numer.value.toUpperCase());
+            db.collection('magazyn').doc('mag4').collection(szukaj.numer.value.toUpperCase()).doc(id).delete();
+
+            //id = '';
+            bgModal.classList.add('hiddenClass');
+            alertSuccess.classList.add('show');
+        })
+
+        stayButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            id = '';
+            bgModal.classList.add('hiddenClass');
+        })
+    })
+    // !-------------------------------------------------------------------------------------------- DELETING DATA
+}
+// !-------------------------------------------------------------------------------------------- RENDER LIST
+
 auth.onAuthStateChanged((user) => {
     if (user) {
-
-        function renderList(doc) {
-            let li = document.createElement('div');
-            // let index = document.createElement('span');
-            let partia = document.createElement('span');
-            let miejsce = document.createElement('span');
-            let trash = document.createElement('i');
-
-            li.setAttribute('data-id', doc.id);
-            li.className = 'list2';
-            miejsce.className = 'miejsce';
-            trash.className = 'bi bi-trash';
-
-            // index.textContent = ai;
-            partia.textContent = doc.data().partia;
-            miejsce.textContent = doc.data().miejsce;
-
-            // li.appendChild(index);
-            li.appendChild(miejsce);
-            li.appendChild(partia);
-            li.appendChild(trash);
-
-            list.appendChild(li);
-
-            //deleting data
-            trash.addEventListener('click', (e) => {
-                e.stopPropagation();
-
-                let id = e.target.parentElement.getAttribute('data-id');
-                bgModal.classList.remove('hiddenClass');
-
-                deleteButton.addEventListener('click', (e) => {
-                    e.preventDefault();
-                    console.log(id);
-
-                    //console.log(szukaj.numer.value.toUpperCase());
-                    db.collection('magazyn').doc('mag4').collection(szukaj.numer.value.toUpperCase()).doc(id).delete();
-
-                    //id = '';
-                    bgModal.classList.add('hiddenClass');
-                    alertSuccess.classList.add('show');
-                })
-
-                stayButton.addEventListener('click', (e) => {
-                    e.preventDefault();
-                    id = '';
-                    bgModal.classList.add('hiddenClass');
-                })
-            })
-        }
-
+        // *-------------------------------------------------------------------------------------------- SEARCH IN DB
         szukaj.addEventListener('submit', (e) => {
             e.preventDefault();
 
+            brak.innerHTML = '';
             list.innerHTML = '';
 
-            let zz = szukaj.numer.value.toUpperCase();
-            console.log(zz)
+            let indexValue = szukaj.numer.value.toUpperCase();
 
-            db.collection('magazyn').doc('mag4').collection(zz).get().then((snapshot) => {
-                snapshot.docs.forEach(doc => {
-                    //console.log(doc.data());
-                    renderList(doc, szukaj.numer.value)
-                })
-            });
+            addIndexFn(indexValue);
 
-            //  real time listener
+            addIndex.classList.remove('hiddenClass');
+            addIndex.innerHTML = 'Dodaj ' + indexValue;
+            
 
-            db.collection('magazyn').doc('mag4').collection(zz).onSnapshot(snapshot => {
+            // *-------------------------------------------------------------------------------------------- REAL TIME LISTENER
+            db.collection('magazyn').doc('mag4').collection(indexValue).onSnapshot(snapshot => {
                 let changes = snapshot.docChanges();
-                // console.log(changes);
                 changes.forEach(change => {
-                    console.log(change.doc.data())
-                    if (change.type == 'removed') {
+                    console.log(change.type);
+                    if (change.type === 'removed') {
                         let li = list.querySelector('[data-id=' + change.doc.id + ']');
                         list.removeChild(li);
                     }
+                    if (change.type === 'added') {
+                        renderListFn(change.doc)
+                        console.log(change.doc)
+                    }
                 })
             })
+            // !-------------------------------------------------------------------------------------------- REAL TIME LISTENER
         })
+        // !-------------------------------------------------------------------------------------------- SEARCH IN DB
 
 
     } else {
